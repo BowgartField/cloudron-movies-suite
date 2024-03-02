6b0b0750963dbf518828190c52da8b762f7c60d9
jeanbenoit-richez <jeanbenoit-richez@outlook.com>
2024-03-02 15:52:22 +0000

    fix: calibre server

--- calibre/start.sh
+++ calibre/start.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eu
 
+dir=/app/data/config/library
+if [[ ! -e $dir ]]; then
+    mkdir $dir
+fi
+
+if [[ ! -e $dir/metadata.db  ]]; then
+    touch $dir/metadata.db
+fi
+
+
 echo "Starting Calibre..."
-exec /app/code/calibre/calibre
+exec /app/code/calibre/calibre-server /app/data/config/library --disable-use-bonjour
