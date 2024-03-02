5a11e36aca5e6abc8aef06159c2d3fa707ac24e4
Jean-Benoît <jeanbenoit@realt.co>
2024-03-02 18:20:48 +0200

    fix: calibre

--- calibre/start.sh
+++ calibre/start.sh
@@ -11,8 +11,12 @@ if [[ ! -e $dir/metadata.db  ]]; then
     touch $dir/metadata.db
 fi
 
+if [[ ! -e /app/data/config/users.sqlite ]]; then
+    mv /app/code/calibre/users.sqlite /app/data/config/users.sqlite
+fi
+
 # CREATE database
-printf '%s\n' 1 admin changeme changeme | /app/code/calibre/calibre-server --userdb /app/data/config/users.sqlite --manage-users
+# printf '%s\n' 1 admin changeme changeme | /app/code/calibre/calibre-server --userdb /app/data/config/users.sqlite --manage-users
 
 echo "Starting Calibre..."
 exec /app/code/calibre/calibre-server /app/data/config/library --userdb /app/data/config/users.sqlite --enable-auth --disable-use-bonjour
