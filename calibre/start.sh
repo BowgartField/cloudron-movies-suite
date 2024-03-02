3682ed7d743aad24ebf02b6d81d64340d7164339
jeanbenoit-richez <jeanbenoit-richez@outlook.com>
2024-03-02 16:18:12 +0000

    feat: improve calibre

--- calibre/start.sh
+++ calibre/start.sh
@@ -11,6 +11,8 @@ if [[ ! -e $dir/metadata.db  ]]; then
     touch $dir/metadata.db
 fi
 
+# CREATE database
+printf '%s\n' 1 admin changeme changeme | /app/code/calibre/calibre-server --userdb /app/data/config/users.sqlite --manage-users
 
 echo "Starting Calibre..."
-exec /app/code/calibre/calibre-server /app/data/config/library --disable-use-bonjour
+exec /app/code/calibre/calibre-server /app/data/config/library --userdb /app/data/config/users.sqlite --enable-auth --disable-use-bonjour
