fa4e81035a32bc3a552568d3d7fa8428ece5239f
Jean-Benoît <jeanbenoit@realt.co>
2024-03-04 16:48:54 +0200

    fix: calibre

--- calibre/start.sh
+++ calibre/start.sh
@@ -2,21 +2,32 @@
 
 set -eu
 
-dir=/app/data/config/library
-if [[ ! -e $dir ]]; then
-    mkdir $dir
+config_path=/app/data/config/config.config
+# Create the directory if it doesn't exist
+if [[ ! -e $config_path ]]; then
+    cp /app/code/config.config $config_path
 fi
 
-if [[ ! -e $dir/metadata.db  ]]; then
-    touch $dir/metadata.db
+db_path=/app/data/config/library
+
+# Get "db_path" var from the config file
+. /app/data/config/config.config
+
+# Create the directory if it doesn't exist
+if [[ ! -e $db_path ]]; then
+    mkdir $db_path
+fi
+
+if [[ ! -e $db_path/metadata.db  ]]; then
+    touch $db_path/metadata.db
 fi
 
 if [[ ! -e /app/data/config/users.sqlite ]]; then
-    mv /app/code/calibre/users.sqlite /app/data/config/users.sqlite
+    mv /tmp/users.sqlite /app/data/config/users.sqlite
 fi
 
 # CREATE database
 # printf '%s\n' 1 admin changeme changeme | /app/code/calibre/calibre-server --userdb /app/data/config/users.sqlite --manage-users
 
 echo "Starting Calibre..."
-exec /app/code/calibre/calibre-server /app/data/config/library --userdb /app/data/config/users.sqlite --enable-auth --disable-use-bonjour
+exec /app/code/calibre/calibre-server $db_path --userdb /app/data/config/users.sqlite --enable-auth --disable-use-bonjour
