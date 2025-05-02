08d40732f0ea23d55cdc8119879c8e6d55811298
Jean-Benoît <jeanbenoit@realt.co>
2025-05-02 06:53:39 +0300

    fix: Bazarr

--- bazarr/start.sh
+++ bazarr/start.sh
@@ -4,10 +4,5 @@ set -eu
 
 echo "Starting Bazarr..."
 
-# Create directories in /app/config if they don't exist
-mkdir -p /app/config/data
-mkdir -p /app/config/logs
-mkdir -p /app/config/config
-
 # Start Bazarr
-exec python3 /app/code/bazarr/bazarr.py
+exec python3 /app/code/bazarr/bazarr.py --config=/app/data/config
